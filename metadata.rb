7f9f2bac036018d40aeb4c62d00f6ccdaa141f99
Robert Reilly <RobertJReilly@gmail.com>
2016-12-24 15:44:12 -0500

    Init working logic

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'all_rights'
 description 'Installs/Configures demonops'
 long_description 'Installs/Configures demonops'
 version '0.1.0'
-
+depends 'firewall'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/demonops/issues' if respond_to?(:issues_url)
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,13 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+chef_gem 'colorize' do
+  action :install
+  compile_time true
+end
+
+require 'colorize'
+
 query = "hostname:#{node['hostname']}"
 farm = search('demonops', query) # ~FC003
 if farm.count == 0
@@ -11,57 +18,72 @@ else
   farm = farm[0].raw_data
   puts "found farm #{farm}"
 end
+
+
+server = ''
 me = ''
 servers = {}
-mytype = 'fail'
+mytypes = Array.new
+mytype = ''
 servertypes = %w(servertype_lb servertype_rabbitmq servertype_redis
                  servertype_sensu servertype_influxdb
                  servertype_statsd servertype_grafana servertype_analysis)
 
+
+
 servertypes.each do |servertype|
-  puts "found * #{servertype}"
+ puts "found * #{servertype}"
   servers[servertype] = []
   if farm.include? (servertype) then
     puts "found ** #{servertype}"
     farm[servertype].each do |server|
-      puts "found *** #{server}"
+      puts "found *** #{server}".green
       servers[servertype] << server["hostname"]
       if node["hostname"].eql? (server["hostname"]) then
-        mytype = servertype
-        puts "found **** #{mytype}"
+        puts "#{servertype}".yellow
+        mytypes.push(servertype)
+        mytype = mytypes.last
+        puts "found **** #{mytype}".green
         me = server
       end
     end
   end
 end
 
-servertypes.each do |type|
+mytypes.each do |type|
   case type
   when "servertype_lb"
-    puts "found ***** #{type}"
+  puts "Selecting Server Type #{type}".blue
 
   when "servertype_rabbitmq"
-    puts "found ***** #{type}"
+  puts "Selecting Server Type #{type}".blue
 
   when "servertype_redis"
-    puts "found ***** #{type}"
+  puts "Selecting Server Type #{type}".blue
 
   when "servertype_sensu"
-    puts "found ***** #{type}"
+  puts "Selecting Server Type #{type}".blue
+  ############################
+  #Fire wall rules for everyone
+  ############################
 
-  when "servertype_influxdb"
-    puts "found ***** #{type}"
-
-  when "servertype_statsd"
-    puts "found ***** #{type}"
-
-  when "servertype_grafana"
-    puts "found ***** #{type}"
-
-  when "servertype_analysis"
-    puts "found ***** #{type}"
-
-  end
+  me['ports'].each do |port|
+     firewall_rule "Port#{port}" do
+       port     port.to_i
+       command  :allow
+     end
 end
 
+  when "servertype_influxdb"
+  puts "Selecting Server Type #{type}".blue
 
+  when "servertype_statsd"
+  puts "Selecting Server Type #{type}".blue
+
+  when "servertype_grafana"
+  puts "Selecting Server Type #{type}".blue
+
+  when "servertype_analysis"
+  puts "Selecting Server Type #{type}".blue
+  end
+end
